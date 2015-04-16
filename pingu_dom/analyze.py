5c5f20d0c1bb3c2f98732e16e0a497d63d15f8bf
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-04-16 14:55:38 -0400

    added analysis script

--- pingu_dom/analyze.py
+++ pingu_dom/analyze.py
@@ -3,20 +3,29 @@ import ROOT
 ROOT.gSystem.Load('libRATEvent')
 from ROOT.RAT import DSReader
 
-ds = DSReader("test.root")
-nevents = ds.GetTotal()
+def fillhist( filename, hnpe ):
 
+    ds = DSReader(filename)
+    nevents = ds.GetTotal()
+
+    for i in xrange(0,nevents):
+        r = ds.NextEvent()
+        mc = r.GetMC()
+
+        posv = mc.GetMCParticle(0).GetPosition()
+        mumomv = mc.GetMCParticle(0).GetMomentum()
+        dir = mumomv.Unit()
+
+        norm = -1.0*posv.Unit()
+        cosv = norm.X()*dir.X() + norm.Y()*dir.Y() + norm.Z()*dir.Z()
+
+        if cosv>0.0:
+            hnpe.Fill( r.GetMC().GetNumPE() )
+        print "event ",i,": pos=",posv.X(),",",posv.Y(),",",posv.Z()," :  dir=",dir.X(),dir.Y(),dir.Z()," : ",cosv
+
+filename = "test.root"
 hnpe = ROOT.TH1D("hnpe","",50, 0, 500)
-
-for i in xrange(0,nevents):
-    r = ds.NextEvent()
-    hnpe.Fill( r.GetMC().GetNumPE() )
-    mc = r.GetMC()
-    
-    posv = mc.GetMCParticle(0).GetPosition()
-    mumomv = mc.GetMCParticle(0).GetMomentum()
-    dir = mumomv.Unit()
-    print "event ",i,": pos=",posv.X(),",",posv.Y(),",",posv.Z()," :  dir=",dir.X(),dir.Y(),dir.Z()
+fillhist( filename, hnpe )
 
 c = ROOT.TCanvas("c","c",800,400)
 c.Draw()
