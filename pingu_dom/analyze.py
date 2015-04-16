401e5ec6ceb57b466272ad2fcca24274026e5e34
Taritree M Wongjirad <taritree@hibat0003.cmsaf.mit.edu>
2015-04-16 15:11:57 -0400

    save

--- pingu_dom/analyze.py
+++ pingu_dom/analyze.py
@@ -3,7 +3,7 @@ import ROOT
 ROOT.gSystem.Load('libRATEvent')
 from ROOT.RAT import DSReader
 
-def fillhist( filename, hnpe ):
+def fillhist( filename, hnpe, hnpe_cos ):
 
     ds = DSReader(filename)
     nevents = ds.GetTotal()
@@ -18,20 +18,32 @@ def fillhist( filename, hnpe ):
 
         norm = -1.0*posv.Unit()
         cosv = norm.X()*dir.X() + norm.Y()*dir.Y() + norm.Z()*dir.Z()
+        npe = r.GetMC().GetNumPE()
 
         if cosv>0.0:
-            hnpe.Fill( r.GetMC().GetNumPE() )
-        print "event ",i,": pos=",posv.X(),",",posv.Y(),",",posv.Z()," :  dir=",dir.X(),dir.Y(),dir.Z()," : ",cosv
+            hnpe.Fill( npe )
+            hnpe_cos.Fill( cosv, npe )
+        #print "event ",i,": pos=",posv.X(),",",posv.Y(),",",posv.Z()," :  dir=",dir.X(),dir.Y(),dir.Z()," : ",cosv
 
-filename = "test.root"
-hnpe = ROOT.TH1D("hnpe","",50, 0, 500)
-fillhist( filename, hnpe )
+
+out = ROOT.TFile("output.root","recreate")
+hnpe_cos = ROOT.TH2D( "hnpe_v_cos", "", 50, 0, 1.0, 50, 0, 500 )
+hnpe = ROOT.TH1D("hnpe","",500, 0, 500)
+
+files = os.listdir( "/net/nudsk0001/d00/scratch/taritree/pingudom/" )
+for f in files:
+    if ".root" not in f:
+        continue
+    filename = "/net/nudsk0001/d00/scratch/taritree/pingudom/"+f
+    print filename
+    fillhist( filename, hnpe, hnpe_cos )
 
 c = ROOT.TCanvas("c","c",800,400)
 c.Draw()
 hnpe.Draw()
 c.Update()
 
+out.Write()
 raw_input()
 
 
