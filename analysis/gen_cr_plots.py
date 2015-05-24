a4cf0be5992a12ed787ef7c3e32b39c9d0537595
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-05-23 20:34:15 -0400

    modified veto geometry

--- analysis/gen_cr_plots.py
+++ analysis/gen_cr_plots.py
@@ -4,25 +4,24 @@ import array
 
 gStyle.SetOptStat(0)
 
-#elasped_time = 1.17161308983*0.5 # seconds
-elasped_time = 0.586272565374 # seconds
+elasped_time = 1.17161308983 # seconds
 scale = 1.0/elasped_time
 search_window = 250.0e-9 # ns
 
-folder="veto_wdark"
-#folder="nodark"
+#folder="veto_wdark"
+folder="veto_nodark"
 os.system("mkdir -p figs/%s/eps"%(folder))
 
-if folder=="nodark":
-    tf = TFile("rootfiles/crana_merged_0_1999.root")
+if folder=="veto_nodark":
+    tf = TFile("crana_merged_nodarknoise_0_1999.root")
 else:
-    tf = TFile("crana_merged_wdarknoise_0_1000.root") # darknoise
+    tf = TFile("crana_merged_wdarknoise_0_1999.root") # darknoise
 mcdata = tf.Get("mcdata")
 
 pe_scale = 4500.0/8500.0
 mcdata.SetAlias("pescale","4500.0/8500.0")
-mcdata.SetAlias("standard_cuts","npulses==2 && pulsepe[1]*pescale>5.0 && abs(pulsez[0]-pulsez[1])<200.0 && pulsepe[0]*pescale>600 && pulsepe[0]*pescale<1300 && pulsepe[1]*pescale<800.0 && npulses_veto==0 && pulse_totodpe<1.0")
-mcdata.SetAlias("nood_cuts",    "npulses==2 && pulsepe[1]*pescale>5.0 && abs(pulsez[0]-pulsez[1])<200.0 && pulsepe[0]*pescale>600 && pulsepe[0]*pescale<1300 && pulsepe[1]*pescale<800.0")
+mcdata.SetAlias("standard_cuts","npulses==2 && abs(pulsez[0]-pulsez[1])<200.0 && pulsepe[0]*pescale>600 && pulsepe[0]*pescale<1300 && pulsepe[1]*pescale>50.0 && pulsepe[1]*pescale<800.0 && pulse_totodpe*pescale<=0")
+mcdata.SetAlias("nood_cuts",    "npulses==2 && abs(pulsez[0]-pulsez[1])<200.0 && pulsepe[0]*pescale>600 && pulsepe[0]*pescale<1300 && pulsepe[1]*pescale>50.0 && pulsepe[1]*pescale<800.0")
 mcdata.SetAlias("nobounds_cuts","pulsepe[0]*pescale>10.0 && pulsepe[1]*pescale>5.0 && abs(pulsez[0]-pulsez[1])<200.0")
 
 
@@ -50,7 +49,7 @@ for h in [h_idpe,h_idpe_zoom]:
     h.GetYaxis().SetTitleSize(0.06)
     h.GetYaxis().SetTitleOffset(1.2)
 
-mcdata.Draw("pescale*pulsepe[0]>>h_idpe","")
+mcdata.Draw("pescale*pulsepe[0]>>h_idpe","npulses>0")
 c1.Update()
 c1.SaveAs("figs/%s/h_idpe.pdf"%(folder))
 c1.SaveAs("figs/%s/eps/h_idpe.eps"%(folder))
@@ -69,9 +68,14 @@ for h in [h_idpe,h_idpe_zoom,h_idpe_wcuts]:
 
 # CR CONTENT
 hncr = []
-for ncr in ["ncr_muons","ncr_photons","ncr_electrons"]:
+ncr_cuts = {"ncr_muons":"ncr_muons>0 && ncr_photons==0 && ncr_electrons==0 && ncr_neutrons==0",
+            "ncr_photons":"ncr_muons==0 && ncr_photons>0 && ncr_electrons==0 && ncr_neutrons==0",
+            "ncr_electrons":"ncr_muons==0 && ncr_photons==0 && ncr_electrons>0 && ncr_neutrons==0",
+            "ncr_neutrons":"ncr_muons==0 && ncr_photons==0 && ncr_electrons==0 && ncr_neutrons>0" }
+
+for ncr in ["ncr_muons","ncr_photons","ncr_electrons","ncr_neutrons"]:
     h = TH1D("h_%s"%(ncr),";pe in prompt pulse; fraction of events",10,0,10)
-    mcdata.Draw("%s>>h_%s"%(ncr,ncr),"standard_cuts")
+    mcdata.Draw("%s>>h_%s"%(ncr,ncr),"standard_cuts && %s"%(ncr_cuts[ncr]))
     c1.Update()
     c1.SaveAs("figs/%s/h_%s.pdf"%(folder,ncr))
 
@@ -79,7 +83,7 @@ for ncr in ["ncr_muons","ncr_photons","ncr_electrons"]:
 # Michel Pulse PE
 c1.cd().SetRightMargin(0.05)
 c1.cd().SetLeftMargin(0.15)
-h_michel_scale = TH1D("h_michel_scale",";pe in Michel pulse; fraction of events",80,0,800)
+h_michel_scale = TH1D("h_michel_scale",";pe in Michel pulse; fraction of events",160,0,800)
 h_michel_scale.GetXaxis().SetLabelSize(0.05)
 h_michel_scale.GetXaxis().SetTitleSize(0.06)
 h_michel_scale.GetXaxis().SetTitleOffset(1.2)
@@ -87,16 +91,17 @@ h_michel_scale.GetYaxis().SetLabelSize(0.05)
 h_michel_scale.GetYaxis().SetTitleSize(0.06)
 h_michel_scale.GetYaxis().SetTitleOffset(1.2)
 #h_michel_scale.GetZaxis().SetTitleSize(0.05)
-mcdata.Draw("pescale*pulsepe[1]>>h_michel_scale","npulses>1")
-h_michel_scale.Scale( 1.0/h_michel_scale.Integral() )
+mcdata.Draw("pescale*pulsepe[1]>>h_michel_scale","npulses==2")
+h_michel_scale.Scale( scale )
 c1.Update()
 c1.SaveAs("figs/%s/h_michel_scale.pdf"%(folder))
 c1.SaveAs("figs/%s/eps/h_michel_scale.eps"%(folder))
 
 # OD PE
+c1.cd().SetLogy(1)
 c1.cd().SetRightMargin(0.05)
 c1.cd().SetLeftMargin(0.15)
-h_odpe = TH1D("h_odpe",";pe in veto pulses; pe/bin/sec",50,0,100)
+h_odpe = TH1D("h_odpe",";pe in veto pulses; pe/bin/sec",100,0,100)
 h_odpe.GetXaxis().SetLabelSize(0.05)
 h_odpe.GetXaxis().SetTitleSize(0.06)
 h_odpe.GetXaxis().SetTitleOffset(1.2)
@@ -104,19 +109,17 @@ h_odpe.GetYaxis().SetLabelSize(0.05)
 h_odpe.GetYaxis().SetTitleSize(0.06)
 h_odpe.GetYaxis().SetTitleOffset(1.2)
 #h_odpe.GetZaxis().SetTitleSize(0.05)
-if folder=="nodark":
-    mcdata.Draw("pescale*odpe>>h_odpe","stadard_cuts && odpe>0")
-else:
-    mcdata.Draw("pescale*pulse_totodpe>>h_odpe","nood_cuts")
+mcdata.Draw("pulse_totodpe>>h_odpe","nood_cuts")
 h_odpe.Scale( scale )
 c1.Update()
 c1.SaveAs("figs/%s/h_odpe.pdf"%(folder))
 c1.SaveAs("figs/%s/eps/h_odpe.eps"%(folder))
+c1.cd().SetLogy(0)
 
 # OD Pulse Z
 c1.cd().SetRightMargin(0.05)
 c1.cd().SetLeftMargin(0.15)
-h_odz = TH1D("h_odz",";pe in veto pulses; pe/bin/sec",100,-4800,4800)
+h_odz = TH1D("h_odz",";pe in veto pulses; pe/bin/sec",100,-5000,5000)
 h_odz.GetXaxis().SetLabelSize(0.05)
 h_odz.GetXaxis().SetTitleSize(0.06)
 h_odz.GetXaxis().SetTitleOffset(1.2)
@@ -124,10 +127,7 @@ h_odz.GetYaxis().SetLabelSize(0.05)
 h_odz.GetYaxis().SetTitleSize(0.06)
 h_odz.GetYaxis().SetTitleOffset(1.2)
 #h_odpe.GetZaxis().SetTitleSize(0.05)
-if folder=="nodark":
-    mcdata.Draw("pescale*odpe>>h_odz","npulses_veto>0 && ")
-else:
-    mcdata.Draw("pulsez_veto[0]>>h_odz","standard_cuts") # implicit cut, found od pulse
+mcdata.Draw("pescale*odpe>>h_odz","nood_cuts")
 h_odz.Scale( scale )
 c1.Update()
 c1.SaveAs("figs/%s/h_odz.pdf"%(folder))
